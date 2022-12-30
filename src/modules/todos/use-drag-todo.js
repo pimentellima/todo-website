93a36cceb44015b0160a4b9243b2f2e977cb2d31
mathe <mattheus_dere@hotmail.com>
2022-12-30 17:33:28 -0300

    .

---
+++ src/modules/todos/use-drag-todo.js
@@ -0,0 +1,83 @@
+import { useContext } from 'react'
+
+import { DragTodoContext } from '../../context/drag-todo-context'
+import { useData } from '../../hooks/use-data';
+
+export const useDragTodo = () => {
+    const { userTodos, setUserTodos } = useData();
+
+    const { dragItem, dragNode, dragging, setDragging } = useContext(DragTodoContext)
+
+    const onDragStart = (e, params) => {  
+        dragNode.current = e.target;
+        
+        dragItem.current = params;
+        
+        dragNode.current.addEventListener('dragend', onDragEnd);
+        
+        setTimeout(() => {
+            setDragging(true)
+        }, 0);
+    }    
+    
+    const onDragEnterCard = (e, params) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const fromIndex = dragItem.current.index
+        const fromSectionIndex = dragItem.current.sectionIndex
+        const toIndex = params.index
+        const toSectionIndex = params.sectionIndex 
+
+        if(toIndex === fromIndex && toSectionIndex === fromSectionIndex) return
+
+        const newTodos = [...userTodos];
+        const fromSection = newTodos[fromSectionIndex];
+        const toSection = newTodos[toSectionIndex];
+        const fromItem = fromSection.content[fromIndex];
+
+        fromSection.content.splice(fromIndex, 1);
+        toSection.content.splice(toIndex, 0, fromItem);
+
+        newTodos.splice(fromSectionIndex, 1, fromSection);
+        newTodos.splice(toSectionIndex, 1, toSection);
+        
+        setUserTodos(newTodos);
+        
+        dragItem.current = params;
+    }    
+
+    const onDragEnterSection = (e, toSectionIndex) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const fromIndex = dragItem.current.index
+        const fromSectionIndex = dragItem.current.sectionIndex
+
+        if (toSectionIndex === fromSectionIndex) return
+
+        const newTodos = [...userTodos];
+        const fromSection = newTodos[fromSectionIndex]
+        const toSection = newTodos[toSectionIndex];
+        const fromItem = fromSection.content[fromIndex];
+
+        fromSection.content.splice(fromIndex, 1);
+        toSection.content.push(fromItem);
+
+        newTodos.splice(fromSectionIndex, 1, fromSection);
+        newTodos.splice(toSectionIndex, 1, toSection);
+
+        dragItem.current = {index: toSection.content.length - 1, sectionIndex: toSectionIndex}
+
+        setUserTodos(newTodos)
+    } 
+    
+    const onDragEnd = () => {
+        dragNode.current.removeEventListener('dragend', onDragEnd);
+        dragNode.current = null;
+        dragItem.current = null;
+        setDragging(false);
+    }    
+
+    return { dragItem, dragging, onDragStart, onDragEnterCard, onDragEnterSection }
+}
